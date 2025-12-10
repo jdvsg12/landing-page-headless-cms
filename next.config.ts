557c49e0f0538c90c852281df2c7a5b4c775e501
Julian Velandia <jdvs_g12@hotmail.com>
2025-12-09 21:59:10 -0500

    fix: next.config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  i18n: {
-    locales: ['en', 'fr', 'es'],
-    defaultLocale: 'es'
-  }
+
 }
 
 export default nextConfig
